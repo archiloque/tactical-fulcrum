b3583dd8e6d1f8cfac317501a6cbb74f0bba0cd1
Julien Kirch <code@archiloque.net>
2024-06-03 22:32:44 +0200

    no message

--- esbuild.build.mjs
+++ esbuild.build.mjs
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
+import fs from "fs"
 import * as esbuild from "esbuild"
 import createConfig from "./esbuild.common.mjs"
 
+console.info("Clearing dir")
+fs.rmSync("out", { recursive: true, force: true });
+
+console.info("Build game")
 console.log(await esbuild.build(await createConfig("game")))
+console.info("Build editor")
 console.log(await esbuild.build(await createConfig("editor")))

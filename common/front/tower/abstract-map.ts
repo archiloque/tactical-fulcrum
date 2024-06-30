41b8a7c5928cfc87ba439f8ae2b43a47f5f49012
Julien Kirch <code@archiloque.net>
2024-06-30 17:00:36 +0200

    no message

--- common/front/tower/abstract-map.ts
+++ common/front/tower/abstract-map.ts
@@ -10,17 +10,19 @@ import { Spriter } from "../map/spriter"
 export abstract class AbstractMap {
   static readonly TOOL_TIP_ID = "abstractMapToolTip"
   static readonly TOOL_TIP_TIP_ID = "abstractMapToolTipTip"
+  static readonly TOOL_TIP_TIP_SLOT_ID = "abstractMapToolTipSlot"
 
   private readonly lastMousePosition: Point = new Point()
   protected lastMouseTile: Point = new Point(-1, -1)
   protected readonly background: Sprite
   protected readonly cursor: Graphics
   protected readonly spriter: Spriter = new Spriter("map")
-  tileSize: number = TILES_DEFAULT_SIZE
-  protected toolTip: HTMLElement
-  protected toolTipTimeout: number | null = null
-  protected tooltipTip: SlTooltip
+  protected tileSize: number = TILES_DEFAULT_SIZE
+  private toolTip: HTMLElement
+  private toolTipTimeout: number | null = null
+  private toolTipTip: SlTooltip
   readonly app: Application
+  private toolTipSlot: HTMLElement
 
   protected constructor() {
     this.app = new Application()
@@ -60,6 +62,7 @@ export abstract class AbstractMap {
       this.background.height = appSize
       this.cursor.scale = this.tileSize / TILES_DEFAULT_SIZE
       this.toolTip.style.width = `${newTileSize}px`
+      this.toolTip.style.height = `${newTileSize}px`
       return this.spriter.reload(this.tileSize).then(() => this.repaint())
     }
   }
@@ -84,15 +87,15 @@ export abstract class AbstractMap {
   public postInit(): void {
     console.debug("AbstractMap", "postInit")
     this.toolTip = document.getElementById(AbstractMap.TOOL_TIP_ID)!
-    this.tooltipTip = document.getElementById(AbstractMap.TOOL_TIP_TIP_ID) as SlTooltip
+    this.toolTipTip = document.getElementById(AbstractMap.TOOL_TIP_TIP_ID) as SlTooltip
+    this.toolTipSlot = document.getElementById(AbstractMap.TOOL_TIP_TIP_SLOT_ID) as SlTooltip
   }
 
   public toolTipHole(): Hole {
-    return html` <div id="${AbstractMap.TOOL_TIP_ID}">
-      <sl-tooltip id="${AbstractMap.TOOL_TIP_TIP_ID}" trigger="manual" hoist content="">
-        <div id="tabMapMapToolTipElement"></div>
-      </sl-tooltip>
-    </div>`
+    return html` <sl-tooltip id="${AbstractMap.TOOL_TIP_TIP_ID}" trigger="manual" hoist>
+      <div slot="content" id="${AbstractMap.TOOL_TIP_TIP_SLOT_ID}"></div>
+      <div id="${AbstractMap.TOOL_TIP_ID}"></div>
+    </sl-tooltip>`
   }
 
   protected schemeChanged(): void {
@@ -127,8 +130,8 @@ export abstract class AbstractMap {
     if (tilePosition.y < TILES_IN_ROW && tilePosition.x < TILES_IN_ROW && !this.lastMouseTile.equals(tilePosition)) {
       console.debug("AbstractMap", "pointerMove", tilePosition)
       this.lastMouseTile = tilePosition
-      if (this.tooltipTip.open) {
-        this.tooltipTip.open = false
+      if (this.toolTipTip.open) {
+        this.toolTipTip.open = false
       }
       this.repositionCursor()
       if (this.toolTipTimeout != null) {
@@ -147,13 +150,13 @@ export abstract class AbstractMap {
     }
     const toolTipText = this.getToolTipText()
     if (toolTipText !== null) {
-      this.tooltipTip.content = toolTipText
+      this.toolTipSlot.innerHTML = toolTipText
       const cursorPosition = this.cursor.getGlobalPosition()
       const top = this.app.canvas.offsetTop + cursorPosition.y
       const left = this.app.canvas.offsetLeft + cursorPosition.x
       this.toolTip.style.top = `${top}px`
       this.toolTip.style.left = `${left}px`
-      this.tooltipTip.open = true
+      this.toolTipTip.open = true
     }
   }
 
